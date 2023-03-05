014a1be44497f6113f5262f9c48a755e3c73fdc9
Alessandro Donnini <info@alessandrodonnini.com>
2023-03-05 11:18:34 +0100

    Add time lines

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,8 @@ export default function Home({ homepage }) {
         {text}
       </p>
       <div className='overflow-auto relative bg-black w-full' ref={scrollRef}>
-        {/* <table> */}
+        <table>
+          <div className='h-6 relative bg-primary w-full gradient-time'></div>
           {data.map((loc)=>{
           return(
             <div className='mb-3 before:absolute before:top-0 before:left-0 before:w-[1px] before:bg-black ' key={loc.name}>
@@ -39,40 +40,38 @@ export default function Home({ homepage }) {
                   {loc.name}
                 </div>
                 <div className='flex flex-nowrap justify-items-start '>
-                  {/* <tr> */}
-                    {loc.events.map((e,i)=>{
-                      let fakeW = widths[Math.floor(Math.random() * widths.length)];
-                      return(
-                        <div style={{ width: fakeW + 'px' }} className={`shrink-0 justify-self-start  sticky top-0 left-1 relative odd:bg-gray even:bg-grayM min-w-[100px] rounded-md`} key={i}>
-                          <div className={`shadow-[-8px_0_0_0_#222] rounded-md h-full inline-block align-top  text-white py-2 `} key={i}>
-                            <div className='absolute top-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-tr-md shadow-[2px_-2px_0_2px_#222]'>
+                  {loc.events.map((e,i)=>{
+                    let fakeW = widths[Math.floor(Math.random() * widths.length)];
+                    return(
+                      <div style={{ width: fakeW + 'px' }} className={`shrink-0 justify-self-start  sticky top-0 left-1 relative odd:bg-gray even:bg-grayM min-w-[100px] rounded-md`} key={i}>
+                        <div className={`shadow-[-8px_0_0_0_#222] rounded-md h-full inline-block align-top  text-white py-2 `} key={i}>
+                          <div className='absolute top-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-tr-md shadow-[2px_-2px_0_2px_#222]'>
+                          </div>
+                          <div className='absolute bottom-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-br-md shadow-[2px_2px_0_2px_#222]'>
+                          </div>
+                          <div className='absolute top-0 left-0 bottom-0 w-[8px] bg-[#66cc66] rounded-l-md'>
+                          </div>
+                          <div style={{maxWidth: size.width - 50 + 'px'}}>
+                            <div className='px-4'>
+                              {e.start_time} - {e.end_time}
                             </div>
-                            <div className='absolute bottom-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-br-md shadow-[2px_2px_0_2px_#222]'>
+                            <div className='px-4 font-bold text-xl'>
+                              {e.title}
                             </div>
-                            <div className='absolute top-0 left-0 bottom-0 w-[8px] bg-[#66cc66] rounded-l-md'>
-                            </div>
-                            <div style={{maxWidth: size.width - 50 + 'px'}}>
-                              <div className='px-4'>
-                                {e.start_time} - {e.end_time}
-                              </div>
-                              <div className='px-4 font-bold text-xl'>
-                                {e.title}
-                              </div>
-                              <div className='px-4 text-sm '>
-                                {e.description}
-                              </div>
+                            <div className='px-4 text-sm '>
+                              {e.description}
                             </div>
                           </div>
                         </div>
-                      )
-                    })}
-                  {/* </tr> */}
+                      </div>
+                    )
+                  })}
                 </div>
               </table>
             </div>
           )
         })}
-        {/* </table> */}
+        </table>
 
       </div>
     </Layout>
