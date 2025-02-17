e7859a1dda4e0df90d50979adb46132eabfc2cd5
Alessandro Donnini <info@alessandrodonnini.com>
2025-02-17 17:01:07 +0100

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,93 +1,15 @@
-import * as queries from 'api/queries';
-import fetchData from 'api/dato';
-import { Image as DatoImage } from 'react-datocms';
-import Layout from 'components/Layout';
-import { useHorizontalScroll } from 'hooks/useSideScroll';
-import data from 'lib/agenda';
-import useWindowSize from 'hooks/useWindowSize'
-export default function Home({ homepage }) {
-  const { title, text, introImage } = homepage
-  const scrollRef = useHorizontalScroll();
-  const widths = [150, 200, 450, 650, 700]
-  const size = useWindowSize();
+import Aurora from "components/Aurora";
 
+export default function Home() {
   return (
-    <Layout {...homepage}>
-      {introImage &&
-        <div>
-          <DatoImage
-            className="dato-image-cover"
-            data={introImage?.responsiveImage}
-            alt={introImage?.responsiveImage.alt}
-            title={introImage?.responsiveImage.title}
-          />
+    <>
+      <Aurora colorStops={["#00d8ff", "#66ff9c", "#00d8ff"]} speed={0.5} />{" "}
+      <div className="h-screen flex items-end justify-start p-10 bg-black text-[#daf6ff] text-5xl text-shadow">
+        <div className="relative z-10">
+          <div>Alessandro</div>
+          <div>Donnini</div>
         </div>
-      }
-      <h1 className="text-6xl font-bold tracking-tighter mt-2">
-        {title}
-      </h1>
-      <p className="mt-4 text-lg">
-        {text}
-      </p>
-      <div className='overflow-auto relative bg-black w-full' ref={scrollRef}>
-        <table>
-          <div className='h-6 relative bg-primary w-full gradient-time'></div>
-          {data.map((loc)=>{
-          return(
-            <div className='mb-3 before:absolute before:top-0 before:left-0 before:w-[1px] before:bg-black ' key={loc.name}>
-              <table className=''>
-                <div className='text-white font-bold uppercase inline-block sticky top-0 left-0 p-3'>
-                  {loc.name}
-                </div>
-                <div className='flex flex-nowrap justify-items-start '>
-                  {loc.events.map((e,i)=>{
-                    let fakeW = widths[Math.floor(Math.random() * widths.length)];
-                    return(
-                      <div style={{ width: fakeW + 'px' }} className={`shrink-0 justify-self-start  sticky top-0 left-1 relative odd:bg-gray even:bg-grayM min-w-[100px] rounded-md`} key={i}>
-                        <div className={`shadow-[-8px_0_0_0_#222] rounded-md h-full inline-block align-top  text-white py-2 `} key={i}>
-                          <div className='absolute top-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-tr-md shadow-[2px_-2px_0_2px_#222]'>
-                          </div>
-                          <div className='absolute bottom-0 -left-[16px] w-[8px] h-[8px] bg-[transparent] rounded-br-md shadow-[2px_2px_0_2px_#222]'>
-                          </div>
-                          <div className='absolute top-0 left-0 bottom-0 w-[8px] bg-[#66cc66] rounded-l-md'>
-                          </div>
-                          <div style={{maxWidth: size.width - 50 + 'px'}}>
-                            <div className='px-4'>
-                              {e.start_time} - {e.end_time}
-                            </div>
-                            <div className='px-4 font-bold text-xl'>
-                              {e.title}
-                            </div>
-                            <div className='px-4 text-sm '>
-                              {e.description}
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    )
-                  })}
-                </div>
-              </table>
-            </div>
-          )
-        })}
-        </table>
-
       </div>
-    </Layout>
-  )
-}
-
-export async function getStaticProps({ locale = 'it', preview }) {
-  const response = await fetchData(queries.home, { locale }, preview);
-  const { site } = await fetchData(queries.site, { locale });
-  const { homepage } = response;
-
-  return {
-    props: {
-      site,
-      homepage,
-      locale,
-    },
-  };
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-
 module.exports = {
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'media', // or 'media' or 'class' or false
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "media", // or 'media' or 'class' or false
   theme: {
     colors: {
       white: "#eee",
-      black: "#222",
+      black: "#111",
       gray: "#333",
       grayM: "#444",
       grayL: "#cccccc",
@@ -13,47 +15,46 @@ module.exports = {
       secondary: "#FD7CFF",
     },
     fontFamily: {
-      'sans': ['Inter'],
+      sans: ["Inter"],
     },
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
     },
     container: {
       padding: {
-        DEFAULT: '1rem',
-        sm: '2rem',
-        lg: '4rem',
-        xl: '5rem',
-        '2xl': '6rem',
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
     extend: {
       spacing: {
-        '128': '32rem',
-        '144': '36rem',
+        128: "32rem",
+        144: "36rem",
       },
       borderRadius: {
-        '4xl': '2rem',
+        "4xl": "2rem",
       },
       transitionProperty: {
-        'height': 'height',
-        'spacing': 'margin, padding',
+        height: "height",
+        spacing: "margin, padding",
       },
       typography: {
         DEFAULT: {
           css: {
-            'max-width': '100%',
-            'p': {
-              'letter-spacing': '.015rem',
-              'line-height': '26px',
+            "max-width": "100%",
+            p: {
+              "letter-spacing": ".015rem",
+              "line-height": "26px",
             },
           },
         },
       },
-    }
+    },
   },
 };
-
